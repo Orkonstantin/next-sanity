bcaa5eabb8fc8a1164d79abdf0c46e697deca2ed
or konstantin <104775108+Orkonstantin@users.noreply.github.com>
2023-04-08 05:50:30 +0300

    changed the css in the layout of the site

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -16,12 +16,18 @@ export default async function RootLayout({
 
   return (
     <html lang="en">
-      <body className="max-w-3xl mx-auto py-10 px-8">
+      <body className="lg:max-w-none md:max-w-3xl mx-auto py-10 px-8">
         <header className="flex items-center justify-between">
-          <Link href="/" className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent text-lg font-bold">OrKon</Link>
+          <Link href="/" className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent text-lg font-bold">
+            <p className='xl:text-3xl lg:text-2xl'>OrKon</p>
+            </Link>
           <div className="flex items-center gap-5 text-sm text-gray-600">
             {pages.map((page) => (
-              <Link key={page._id} href={`/${page.slug}`} className="hover:underline">{page.title}</Link>
+              <Link key={page._id} href={`/${page.slug}`} className="hover:underline">
+                 <p className='xl:text-xl lg:text-lg'>
+                  {page.title}
+                 </p>
+                </Link>
             ))}
 
           </div>
