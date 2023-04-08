3f5689550aeff0b779fd502116d2096a516a102f
or konstantin <104775108+Orkonstantin@users.noreply.github.com>
2023-04-08 03:58:36 +0300

    Changed the home route p text

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -8,7 +8,7 @@ export default async function Home() {
   return (
    <div>
     <h1 className="text-7xl font-extrabold">Hello I&apos;m <span className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent">Or</span>!</h1>
-    <p className="mt-3 text-xl text-gray-600">Aloha everyone! check out my projects!</p>
+    <p className="mt-3 text-xl text-gray-600">Hi everyone! check out my projects!</p>
     <h2 className="mt-24 font-bold text-gray-700 text-3xl">My Projects</h2>
 
     <div className="mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
